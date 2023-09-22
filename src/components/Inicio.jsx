6a720e1eaeadb90b876a8cb23cf9bc2eaf90e8b0
Oscar <ohohurtado16@gmail.com>
2023-09-21 21:01:21 -0500

    Creando el el navbar-izquierda

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,24 +1,36 @@
 import React from "react";
-import '../stylesheets/Inicio.css'
-import logo from '../images/tecsup_logo.png'
+import '../stylesheets/Inicio.css';
+import logo from '../images/tecsup_logo.png';
+import Button_navbar from "./buttons_component/Button_navbar";
 
 function Inicio(){
     return(        
-        <div className="inicio-container">
-            <div className="navbar-header">                
-                <img src={logo} alt="logo-tecsup" id="logo" />                    
-                
-                Este es el navbar main
-            </div>
+        <div className="inicio-container">         
+
+        <div className="left-navbar"></div>  
+     
             <aside>
+                <img src={logo} alt="logo-tecsup" id="logo" />                  
                 <nav>
                     <ul>
-                        <li>Inicio</li>
-                        <li>Horarios</li>
-                        <li>Datos</li>
+                        <Button_navbar texto="INICIO"></Button_navbar>
+                        <Button_navbar texto="INICIO"></Button_navbar>
+                        <Button_navbar texto="INICIO"></Button_navbar>
+                        <Button_navbar texto="INICIO"></Button_navbar>
+                        {/* <button><li>Inicio</li></button><br />
+                        <button><li>Horarios</li></button><br />
+                        <button><li>Datos</li></button> */}
                     </ul>
                 </nav>
             </aside>
+
+        
+           
+           {/*  <div className="navbar-header">                
+                <img src={logo} alt="logo-tecsup" id="logo" />                    
+                
+                Este es el navbar main
+            </div> */}
             
             
         </div>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,34 +1,34 @@
 import React from "react";
 import '../stylesheets/Login.css'
 import logo from '../images/tecsup_logo.png'
-import Inicio from "./Inicio";
+
 import {  BrowserRouter as Router, Routes,  Route,  Link} from "react-router-dom";
 function Login(){
     return(
         
         <div className="login-main">
-        <div className="login-container">
-                <div className="login_header">
-                    <img src={logo} alt="Logo de tecsup" />
-                </div>
-                <div className="login-body">
-                    <form action="">
-                        <label htmlFor="username">Correo electronico</label>
-                        <input type="text" placeholder="admin.tecsup@tecsup.edu.pe" autoFocus="autoFocus" id="username"/> <br />
+            <div className="login-container">           
+                    <div className="login-header">
+                        <img src={logo} alt="Logo de tecsup"  /> 
+                     </div>                                   
+                    <div className="login-body">
+                        <form action="">
+                            <label htmlFor="username">Correo electronico</label>
+                            <input type="text" placeholder="admin.tecsup@tecsup.edu.pe" autoFocus="autoFocus" id="username"/> <br />
 
-                        <label htmlFor="password" className="ic-Input text">Contraseña</label>
-                        <input type="password" placeholder="Password" autoFocus="autoFocus" id="password" /> <br />
+                            <label htmlFor="password" className="ic-Input text">Contraseña</label>
+                            <input type="password" placeholder="Password" autoFocus="autoFocus" id="password" /> <br />
 
-                        <a  target="_blank_" href="https://Facebook.com">¿Olvido la contraseña?</a>
+                            <a  target="_blank_" href="https://Facebook.com">¿Olvido la contraseña?</a>
 
-                        <div className="buttons-container">                          
+                            <div className="buttons-container">                          
+                                    
+                                <Link to="/inicio-admin"><button  className="button-login">INICIAR SESIÓN</button></Link><br/>
+                                <Link to="/inicio-admin" ><button className="button-login-google">Ingresa con tu correo de tecsup</button></Link>
                                 
-                             <Link to="/inicio-admin"><button  className="button-login">INICIAR SESIÓN</button></Link><br/>
-                             <Link to="/inicio-admin" ><button className="button-login-google">Ingresa con tu correo de tecsup</button></Link>
-                            
-                        </div>
-                    </form>           
-                </div>        
+                            </div>
+                        </form>           
+                    </div>        
             </div>
         </div>
             
